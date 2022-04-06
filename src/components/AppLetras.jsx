53fb7e4f912d887254f16df924e6babf899b5183
Armando Capriles <armandotechno12@gmail.com>
2022-04-06 17:44:49 -0400

    primer commit

---
+++ src/components/AppLetras.jsx
@@ -0,0 +1,28 @@
+import Formulario from "./Formulario"
+import Alerta from "./Alerta"
+import Letra from "./Letra"
+import useLetras from "../hooks/useLetras"
+import Spinner from "./Spinner"
+
+const AppLetras = () => {
+
+    const { alerta, letra, cargando} = useLetras()
+
+    return (
+        <>
+            <header>Búsqueda de Letras de Canciones</header>
+
+            <Formulario />
+
+            <main>
+                {alerta ? <Alerta>{alerta}</Alerta> : 
+                letra ? <Letra /> : 
+                cargando ? <Spinner /> :
+                <p className="text-center">Busca letras de tus artistas favoritos</p>
+                }
+            </main>
+        </>
+    )
+}
+
+export default AppLetras
